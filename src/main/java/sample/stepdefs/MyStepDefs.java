7433bc41315cde0e1f586db17dee9f1f3fbcee91
dechrish <dechrish@hotmail.com>
2018-11-17 10:30:27 -0800

    clean code

--- src/main/java/sample/stepdefs/MyStepDefs.java
+++ src/main/java/sample/stepdefs/MyStepDefs.java
@@ -1,7 +1,5 @@
 package sample.stepdefs;
 
-import sample.enums.COLOR;
-import sample.enums.SHAPE;
 import cucumber.api.java.en.Given;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,22 +7,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class MyStepDefs {
 
-
-    @Given("^verify color (.*)$")
-    public void verifyColor(COLOR color) {
-        //Assert.assertEquals(color.toString(),"RED","Test Case passed");
-    }
-
-
-    @Given("^verify shape (.*)")
-    public void verifyShape(SHAPE shape) {
-
-        //Assert.assertEquals(shape.toString(),"TRIANGLE","Test Case passed");
-    }
+    WebDriver driver;
 
     @Given("verify page {}")
     public void verifyPage(String url) {
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
         driver.get(url);
         driver.quit();
 
